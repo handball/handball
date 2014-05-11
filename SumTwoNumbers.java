321957aa5d66062dd72488fcca9ed8a3af0c8134
dr3zz <yavor.todorov23@gmail.com>
2014-05-11 20:31:17 +0300

    SumTwoNumbers
    testjava

---
+++ SumTwoNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class SumTwoNumbers {
+
+	public static void main(String[] args) {
+		// TOnDO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter first number: ");
+		int firstNumber = scan.nextInt();
+		System.out.println("Enter secound number: ");
+		int secondNumber = scan.nextInt();
+		int sum = firstNumber + secondNumber;
+		System.out.println("Sum = " +sum);
+	}
+
+}
