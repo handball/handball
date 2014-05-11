2c461743a86b9f02895d606555ad426b9e70a755
tedovn <n1ke_tedy@abv.bg>
2014-05-11 20:34:20 +0300

    CurrenDateAndTime

---
+++ CurrentDateandTime.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+
+
+public class CurrentDateandTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date currentDate = new Date();
+		System.out.println(currentDate);
+
+	}
+
+}
